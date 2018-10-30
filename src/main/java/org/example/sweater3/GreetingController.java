8b1ca079337e1a34940b5f80bcec1c6ca3f54a71
Valentin666 <lita-stroy@mail.ru>
2018-10-30 16:41:50 +0500

    mustache

--- src/main/java/org/example/sweater3/GreetingController.java
+++ src/main/java/org/example/sweater3/GreetingController.java
@@ -1,17 +1,22 @@
 package org.example.sweater3;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import java.util.Map;
 
 @Controller
 public class GreetingController {
 
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="dobriy chelovek") String name, Model model) {
-        model.addAttribute("name", name);
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="dobriy chelovek") String name, Map<String, Object> model) {
+        model.put("name", name);
         return "greeting";
+    }
+
+    @GetMapping
+    public String main() {
+        return "main";
     }
 
 }
